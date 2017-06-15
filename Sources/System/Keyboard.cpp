e4ca8a6104eea65f0833028ccc4869cd21022add
Shota HAMADA <maku3ts@gmail.com>
2017-06-15 20:59:46 +0900

    Fix violation of ODR

---
+++ Sources/System/Keyboard.cpp
@@ -0,0 +1,37 @@
+#include "System/Keyboard.hpp"
+
+namespace System {
+
+KeyCode translateVKeyToKeyCode(USHORT vKey)
+{
+    switch (vKey) {
+    case VK_ESCAPE:
+        return KeyCode::Esc;
+    case VK_SPACE:
+        return KeyCode::Space;
+    case 0x41:
+        return KeyCode::A;
+    case 0x44:
+        return KeyCode::D;
+    case 0x53:
+        return KeyCode::S;
+    case 0x57:
+        return KeyCode::W;
+    default:
+        return KeyCode::Undefined;
+    }
+}
+
+KeyState translateMessageToKeyState(UINT message)
+{
+    switch (message) {
+    case WM_KEYDOWN:
+        return KeyState::Down;
+    case WM_KEYUP:
+        return KeyState::Up;
+    default:
+        return KeyState::Undefined;
+    }
+}
+
+} // namespace System
